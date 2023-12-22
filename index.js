11ad046e7d56cc5789cda082fff07d7ccc7368ab
Mestre <kadukitesesi@gmail.com>
2023-12-21 22:06:48 -0300

    primeiro

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from  'express'
+;
+import { servicoValidaAno, servicoBuscarFatoPorAno} from './servico.js';
+
+const app = express();
+
+app.get('/', (req, res) =>  {
+    let anoFato = req.query.ano;
+
+    if(servicoValidaAno(anoFato))
+    {
+        var fato = servicoBuscarFatoPorAno(anoFato);
+        res.json({fato:fato});
+    }
+    else
+    {
+        res.status(400).json({erro:'Parâmetro ano inválido'});
+    }
+});
+
+app.listen(8080, () => {
+    console.log("Servidor rodando na porta: 8080");
+});
